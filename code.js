4cb3ac3885f8a822866880709e3406054a216b9e
Andres <103233568+Modric9404@users.noreply.github.com>
2024-04-11 22:26:38 -0600

    Update code.js

--- code.js
+++ code.js
@@ -24,3 +24,57 @@ document.addEventListener('DOMContentLoaded', function() {
       });
   });
 });
+
+
+
+var textos = [
+  "Introducción:",
+  "Andrés Castillo.",
+  "Keren Ariana.",
+  "Uriel Flores.",
+  "El ardilla."
+];
+
+
+var elementoParaEscribir = document.querySelector('.escribir-texto');
+
+
+function escribirTexto(texto, indice, callback) {
+  if (indice < texto.length) {
+      elementoParaEscribir.textContent += texto.charAt(indice);
+      indice++;
+      setTimeout(function() {
+          escribirTexto(texto, indice, callback);
+      }, 100); 
+  } else if (callback) {
+      setTimeout(callback, 1500);
+  }
+}
+
+
+function borrarTexto(callback) {
+  var texto = elementoParaEscribir.textContent;
+  if (texto.length > 0) {
+      elementoParaEscribir.textContent = texto.substring(0, texto.length - 1);
+      setTimeout(function() {
+          borrarTexto(callback);
+      }, 50); 
+  } else if (callback) {
+      callback();
+  }
+}
+
+function escribirYBorrarFrases(i) {
+  if (i < textos.length) {
+      escribirTexto(textos[i], 0, function() {
+          borrarTexto(function() {
+              escribirYBorrarFrases(i + 1); 
+          });
+      });
+  } else {
+      escribirYBorrarFrases(0);
+  }
+}
+
+
+escribirYBorrarFrases(0);
